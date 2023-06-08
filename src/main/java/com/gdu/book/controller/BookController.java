8efb609fe99fcb8ccd96ddcd6c82b4ba2be5d76e
pona8257 <pona8257@naver.com>
2023-06-08 16:14:40 +0900

    hjh

---
+++ src/main/java/com/gdu/book/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.gdu.book.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/book")
+public class BookController {
+
+  
+  @GetMapping("/bookList.html")
+  public String movieList() {
+	  return "book/booklist";
+  }
+  
+
+}
