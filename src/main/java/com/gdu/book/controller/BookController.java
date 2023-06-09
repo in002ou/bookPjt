c95ff5fca6215efa092f760f0b998fae579d116e
pona8257 <pona8257@naver.com>
2023-06-09 16:38:59 +0900

    hjh

--- src/main/java/com/gdu/book/controller/BookController.java
+++ src/main/java/com/gdu/book/controller/BookController.java
@@ -1,9 +1,14 @@
 package com.gdu.book.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gdu.book.service.BookService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,7 +17,8 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/book")
 public class BookController {
 
-  
+  private final BookService bookService; 
+	
   @GetMapping("/bookList.html")
   public String movieList() {
 	  return "book/booklist";
@@ -20,6 +26,11 @@ public class BookController {
   
   @GetMapping("/bookDetail.html")
   public String movieDetail() {
+	  return "book/bookDetail";
+  }
+  @PostMapping("/bookReviewadd.do")
+  public String bookReviewadd(Model model, HttpServletRequest request) {
+	  model.addAttribute("addResult", bookService.addBookReview(request));
 	  return "book/bookDetail";
   }
   
